4ee2bb38e596a3c5432af9f481f78290a5b69abe
jianxin_yang <jianxin_yang@foxitsoftware.com>
2024-03-05 15:58:31 +0800

    refactor example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,65 @@
-import Head from 'next/head';
-import {useEffect} from 'react';
+import Head from "next/head";
+import { useEffect, useRef } from "react";
+
 
 export default function Home() {
-  useEffect(function () {
-    const licenseSN = 'xxx';
-    const licenseKey = 'xxx';
-
-    const readyWorker = preloadJrWorker({
-      workerPath: '/FoxitPDFSDKForWeb/lib/',
-      enginePath: '../lib/jr-engine/gsdk',
-      fontPath: 'http://webpdf.foxitsoftware.com/webfonts/',
-      licenseSN: licenseSN,
-      licenseKey: licenseKey
-    });
-
-    const PDFUI = UIExtension.PDFUI;
-    const pdfui = new PDFUI({
-      viewerOptions: {
-        libPath: '/FoxitPDFSDKForWeb/lib',
-        jr: {
-          readyWorker: readyWorker
-        }
-      },
-      renderTo: '#pdf-ui',
-      appearance: UIExtension.appearances.adaptive,
-      fragments: [],
-      addons: UIExtension.PDFViewCtrl.DeviceInfo.isMobile ?
-        '/FoxitPDFSDKForWeb/lib/uix-addons/allInOne.mobile.js':
-        '/FoxitPDFSDKForWeb/lib/uix-addons/allInOne.js'
-    });
-
-    pdfui.openPDFByHttpRangeRequest({
-      range: {
-        url: '/FoxitPDFSDKforWeb_DemoGuide.pdf',
+  const elementRef = useRef();
+  const pdfuiRef = useRef();
+  useEffect(
+    function () {
+      if (!elementRef.current || !!pdfuiRef.current) {
+        return;
       }
-    }, {fileName: 'FoxitPDFSDKforWeb_DemoGuide.pdf'});
-  }, []);
-
+      const licenseSN = "xxx";
+      const licenseKey = "xxx";
+      const readyWorker = preloadJrWorker({
+        workerPath: "/FoxitPDFSDKForWeb/lib/",
+        enginePath: "../lib/jr-engine/gsdk",
+        fontPath: "http://webpdf.foxitsoftware.com/webfonts/",
+        licenseSN: licenseSN,
+        licenseKey: licenseKey,
+      });
+      const PDFUI = UIExtension.PDFUI;
+      const pdfui = new PDFUI({
+        viewerOptions: {
+          libPath: "/FoxitPDFSDKForWeb/lib",
+          jr: {
+            readyWorker: readyWorker,
+          },
+        },
+        renderTo: elementRef.current,
+        appearance: UIExtension.appearances.adaptive,
+        fragments: [],
+        addons: UIExtension.PDFViewCtrl.DeviceInfo.isMobile
+          ? "/FoxitPDFSDKForWeb/lib/uix-addons/allInOne.mobile.js"
+          : "/FoxitPDFSDKForWeb/lib/uix-addons/allInOne.js",
+      });
+      pdfuiRef.current = pdfui;
+    },
+    [elementRef]
+  );
+  useEffect(() => {
+    if(!pdfuiRef.current) {
+      return;
+    }
+    const pdfui = pdfuiRef.current;
+    pdfui.openPDFByHttpRangeRequest(
+      {
+        range: {
+          url: "/FoxitPDFSDKforWeb_DemoGuide.pdf",
+        },
+      },
+      { fileName: "FoxitPDFSDKforWeb_DemoGuide.pdf" }
+    );
+  }, [pdfuiRef])
   return (
     <>
       <Head>
-        <link rel="stylesheet" type="text/css" href="/FoxitPDFSDKForWeb/lib/UIExtension.css"/>
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="/FoxitPDFSDKForWeb/lib/UIExtension.css"
+        />
         <script src="/FoxitPDFSDKForWeb/lib/UIExtension.full.js"></script>
         <script src="/FoxitPDFSDKForWeb/lib/preload-jr-worker.js"></script>
         <title>FoxitPDFSDKForWeb Next.js Example</title>
@@ -68,7 +87,7 @@ export default function Home() {
           <i className="fv__icon-logo"></i>
         </div>
       </div>
-      <div id="pdf-ui"></div>
+      <div id="pdf-ui" ref={elementRef}></div>
     </>
-  )
+  );
 }
